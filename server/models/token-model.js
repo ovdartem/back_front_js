029e97dbadae61ccd702aaed751c5dc268bd7b2d
Artsem.Audziyenka <Artsem.Audziyenka@itechart-group.com>
2022-02-17 15:59:09 +0300

    create reg function

--- server/models/token-model.js
+++ server/models/token-model.js
@@ -5,4 +5,4 @@ const TokenSchema = new Schema({
     refreshToken: {type: String, required: true}
 })
 
-module.exports = model('User', UserSchema);
+module.exports = model('Token', TokenSchema);
--- server/service/user-service.js
+++ server/service/user-service.js
@@ -1,12 +1,28 @@
 const UserModal = require('../models/user-model')
-const bcrypt = require('bc')
+const bcrypt = require('bcrypt')
+const uuid = require('uuid')
+const mailService = require('./mail-service')
+const tokenService = require('./token-service')
+const UserDto = require('../dtos/user-dto')
+
 class UserService {
     async registration(email, password) {
         const candidate = await UserModal.findOne({email})
-        if(candidate) {
+        if (candidate) {
             throw new Error(`Пользователь с почтовым адрессом ${email} уже существует`)
         }
-        const user = await UserModal.create({email, password})
+        const hashPassword = await bcrypt.hash(password, 3)
+        const activationLink = uuid.v4()
+        const user = await UserModal.create({email, password: hashPassword, activationLink})
+        await mailService.sendActivationMail(email, activationLink)
+        const userDto = new UserDto(user)
+        const tokens = tokenService.generateTokens({...userDto})
+        await tokenService.saveToken(userDto.id, tokens.refreshToken)
+
+        return {
+            ...tokens,
+            user: userDto
+        }
     }
 }
 
